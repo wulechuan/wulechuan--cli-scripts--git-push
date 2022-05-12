bdb941827a1fda3ec87a59eaf5b703a134d93e99
吴乐川 <wulechuan@live.com>
2022-05-12 17:41:32 +0800

    无关痛痒。

--- 用于研发阶段的命令行工具/PowerShell/Add-添加所有集得源.ps1
+++ 用于研发阶段的命令行工具/PowerShell/Add-添加所有集得源.ps1
@@ -6,9 +6,11 @@ if ("$PWD" -match "\\用于研发阶段的命令行工具\\PowerShell`$") {
 
 
 
-Import-Module  '.\源代码\PowerShell\吴乐川-文本处理工具.psm1'
-Import-Module  '.\源代码\PowerShell\吴乐川-文本显示工具.psm1'
-Import-Module  '.\源代码\PowerShell\吴乐川-集得源管理工具集.psm1'
+$吴乐川的模块的路径 = '.\源代码\PowerShell'
+Import-Module  "${吴乐川的模块的路径}\吴乐川-文本处理工具.psm1"
+Import-Module  "${吴乐川的模块的路径}\吴乐川-文本显示工具.psm1"
+Import-Module  "${吴乐川的模块的路径}\吴乐川-集得源管理工具集.psm1"
+
 
 
 Add-吴乐川添加单个集得源  -集得源在本机采用的名称 '吴乐川：码云'  -特征颜色 'DarkRed' `
